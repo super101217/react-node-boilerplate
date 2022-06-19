23707accc54d567fe5371423c8f34932a79bf083
Superstar0107 <am000hw@gmail.com>
2022-06-19 11:01:30 -0700

    Add necessary built-in & external middlewares

--- server/setups/app/index.js
+++ server/setups/app/index.js
@@ -1,7 +1,13 @@
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 
 const createApp = () => {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(express.json());
 
   return app;
 };
